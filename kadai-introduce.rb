c2075788eb2134b800e19b62953459cbf5d2037e
daisuke.nagamatsu <mugiblog5656@gmail.com>
2021-06-23 06:27:51 +0000

    update-file

--- kadai-introduce.rb
+++ kadai-introduce.rb
@@ -2,9 +2,5 @@ myouji = '永松'
 namae = '大輔'
 nenrei = 32
 
-str1 = myouji
-str2 = namae 
-num = nenrei
-
-str = str1 + str2 + 'です。' + num.to_s + '歳です。'
-puts str
+introduce =  + myouji + namae + 'です。' + nenrei.to_s + '歳です。'
+puts introduce
